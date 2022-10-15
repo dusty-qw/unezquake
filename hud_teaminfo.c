bca412862f35a11eea5be5fdac152eca69902612
dusty-qw <71472647+dusty-qw@users.noreply.github.com>
2022-10-15 01:01:04 -0700

    TEAMINFO: more info reporting for CA/WIPEOUT

--- hud_teaminfo.c
+++ hud_teaminfo.c
@@ -775,6 +775,8 @@ void Parse_CAInfo(char *s)
 	ti_clients[client].camode = atoi(Cmd_Argv(13));
 	ti_clients[client].isdead = atoi(Cmd_Argv(14));
 	ti_clients[client].timetospawn = atoi(Cmd_Argv(15));
+	ti_clients[client].round_kills = atoi(Cmd_Argv(16));
+	ti_clients[client].round_deaths = atoi(Cmd_Argv(17));
 }
 
 static void Update_TeamInfo(void)
