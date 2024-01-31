1d52508357930095e905751fd65a3296d99dde0b
dusty-qw <71472647+dusty-qw@users.noreply.github.com>
2024-01-30 16:09:31 -0800

    Revert "Revert "CL_EASYAIRCONTROL: only allowed during prewar""
    This reverts commit 2f5a812d5727ef510cedc2a33203332dd61826b3.

--- src/cl_input.c
+++ src/cl_input.c
@@ -1156,7 +1156,7 @@ void CL_SendCmd(void)
 		Cam_Track(cmd);
 	}
 
-	if (cl_easyaircontrol.value && !(cl.onground))
+	if (cl_easyaircontrol.value && !(cl.onground) && !(cl.waterlevel >= 2) && cl.standby)
 		CL_EasyAirControl(cmd);
 
 	CL_FinishMove(cmd);
